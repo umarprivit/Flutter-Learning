86aaeac59a6116d37f77cc263c31e7d7a2ba82da
creeper <umarprivit@gmail.com>
2024-02-20 14:49:53 +0500

    for demo

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -18,7 +18,7 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
         theme: ThemeData(useMaterial3:false, primarySwatch: Colors.blue),// usematerial3 : false is done because if we use material 3 we cant chage teh whole theme for the app it is done for the compatablity or to apps look native 
         title: "ChatApp",
-        home: loginPage(),
+        home: chatPage(),
         routes: {
            '/chatpage':(context) => chatPage()   //this is anohter way that we called it named routes means we named a route with a key value which treated as his name and can be used   
           
